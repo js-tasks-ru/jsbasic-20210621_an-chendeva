124872dd472ed54963c7d284d2c4291e8d1bf46c
unknown <anastasya.galyamova@gmail.com>
2021-07-02 02:17:40 +0500

    решение 4-task

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,8 @@
-function checkSpam(str) {
-  // ваш код...
+function checkSpam(text) {
+  let regExp = /(1[xX][bB][eE][tT]|[xX]{3})/;
+    if ( regExp.test(text)) {
+      return true;
+    } else {
+      return false;
+    }
 }
