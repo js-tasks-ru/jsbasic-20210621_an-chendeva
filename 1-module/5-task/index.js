d0c1d160d5fd28477bb1081c9763a926666a97bc
unknown <anastasya.galyamova@gmail.com>
2021-07-02 03:13:38 +0500

    решение 5-task

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,8 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  if (str.length > maxlength) {
+    let end = maxlength - 1
+    return str.slice(0,end) + "…"
+    } else {
+    return str
+    }
 }
