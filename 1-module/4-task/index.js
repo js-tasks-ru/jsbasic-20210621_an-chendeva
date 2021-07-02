651230045f5e4167f479252777ea2d02c60d4e06
unknown <anastasya.galyamova@gmail.com>
2021-07-02 20:47:50 +0500

    внесла изменения в решение 4-task

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,8 +1,4 @@
 function checkSpam(text) {
   let regExp = /(1[xX][bB][eE][tT]|[xX]{3})/;
-    if ( regExp.test(text)) {
-      return true;
-    } else {
-      return false;
-    }
+    return regExp.test(text);
 }
