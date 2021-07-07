5bd71ec6a63781fa0eee215e6961f2db435e38e0
unknown <anastasya.galyamova@gmail.com>
2021-07-08 03:14:50 +0500

    решение 2-module 1-task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,21 @@
-function sumSalary(salaries) {
-  // ваш код...
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
 }
+
+function sumSalary(salaries) {
+  sum = 0;
+  for (salary in salaries)
+  {
+    if (parseInt(salaries[salary])) {
+      sum = (sum + salaries[salary]);
+    }
+  }
+  return sum;
+}
+
+sumSalary(salaries);
