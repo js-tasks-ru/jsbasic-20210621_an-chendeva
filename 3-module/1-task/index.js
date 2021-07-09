fcaa41d7d06e5cf98ae0abcb878bad5b06bc726d
unknown <anastasya.galyamova@gmail.com>
2021-07-10 02:18:03 +0500

    решение 3-module 1-task

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,17 @@
+let vasya = { name: 'Вася', age: 25 };
+let petya = { name: 'Петя', age: 30 };
+let masha = { name: 'Маша', age: 28 };
+
+let users = [ vasya, petya, masha ];
+
+
 function namify(users) {
-  // ваш код...
+  let names = [] ;
+  for (loopvar in users)
+  {
+    names.push(users[loopvar]["name"])
+  }
+  return names;
 }
+
+let names = namify(users);
