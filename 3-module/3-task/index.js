c6f3d59151d4fef8985a65958085ecfcd37eb071
unknown <anastasya.galyamova@gmail.com>
2021-07-11 02:14:45 +0500

    решение 3-module 3-task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,16 @@
+function ucFirst(str) {
+  if (!str) return str;
+
+  return str[0].toUpperCase() + str.slice(1);
+}
+
 function camelize(str) {
-  // ваш код...
+  let arr = str.split('-');
+    for (loopvar in arr)
+  {
+    if (loopvar > 0 ) {
+      arr[loopvar] = ucFirst(arr[loopvar]);
+    };
+  }
+  return arr.join('');
 }
