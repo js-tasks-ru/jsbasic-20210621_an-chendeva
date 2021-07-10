47c0b6c4a62d6e9009954c0408cf3dc385d604c5
unknown <anastasya.galyamova@gmail.com>
2021-07-11 00:10:23 +0500

    решение 3-module 2-task

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,14 @@
+let arr = [5, 3, 8, 1];
+
 function filterRange(arr, a, b) {
-  // ваш код...
+  let num = [] ;
+  for (loopvar in arr)
+  {
+    if (arr[loopvar] >= a && arr[loopvar] <= b) {
+      num.push(arr[loopvar])
+    };
+  }
+  return num;
 }
+
+let filtered = filterRange(arr, 1, 4);
