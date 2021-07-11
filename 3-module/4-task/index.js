eeaedf4616932a77755459839075b9e94ad61ae5
unknown <anastasya.galyamova@gmail.com>
2021-07-12 03:23:57 +0500

    3-module 4-task

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,10 @@
 function showSalary(users, age) {
-  // ваш код...
+  let arr = [];
+  for (loopvar in users)
+{
+  if (users[loopvar]["age"] <= age) {
+    arr.push(users[loopvar]["name"] + ", " + users[loopvar]["balance"]);
+  };
+}
+return arr.join('\n');
 }
