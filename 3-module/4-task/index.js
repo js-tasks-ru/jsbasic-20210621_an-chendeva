5d20db1e7599b50837d25b7a4d01badee7bf0aa6
unknown <anastasya.galyamova@gmail.com>
2021-07-12 22:58:39 +0500

    решение 3-module 5-task

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,7 +1,7 @@
 function showSalary(users, age) {
   let arr = [];
   for (loopvar in users)
-{
+  {
   if (users[loopvar]["age"] <= age) {
     arr.push(users[loopvar]["name"] + ", " + users[loopvar]["balance"]);
   };
