9e86bf7f46163e56236594927e1fb242e50bb738
unknown <anastasya.galyamova@gmail.com>
2021-07-17 02:03:45 +0500

    внесла изменения в решение 3-module 5-task

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,19 +1,4 @@
-const inputData = '1, -5.8 или 10, хотя 34 + -5.3 и 73'; 
-
 function getMinMax(str) {
-  let arr = [];
-  let strarr = str.split(/,| /);
-    for (loopvar in strarr)
-    {
-    if (parseInt(strarr[loopvar])) {
-        arr.push(strarr[loopvar] * 1);
-    };
-  }
-  arr = arr.sort((a, b) => a - b);
-  arr[0];
-  arr[arr.length - 1];
-
-  return {min: arr[0], max: arr[arr.length - 1]}
+  let arr = str.split(/,| /).filter(item => parseInt(item)).map(item => item * 1).sort((a, b) => a - b);
+  return {min: arr[0], max: arr[arr.length - 1]};
 }
-
-getMinMax(inputData)
