14475c2f11fa773450104779b452be6ccb882c4b
unknown <anastasya.galyamova@gmail.com>
2021-07-16 22:44:44 +0500

    внесла изменения в решение 3-module 3-task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,16 +1,3 @@
-function upperCase(str) {
-  if (!str) return str;
-
-  return str[0].toUpperCase() + str.slice(1);
-}
-
 function camelize(str) {
-  let arr = str.split('-');
-    for (loopvar in arr)
-  {
-    if (loopvar > 0 ) {
-      arr[loopvar] = upperCase(arr[loopvar]);
-    };
-  }
-  return arr.join('');
+  return str.split('-').map((word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)).join('');
 }
