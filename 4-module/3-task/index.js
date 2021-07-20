1fe3e9cf5bc0e74aa9a68eb491ad2b15fdf47bd4
unknown <anastasya.galyamova@gmail.com>
2021-07-21 02:05:35 +0500

    решение 4-module 3-task

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,32 @@
 function highlight(table) {
-  // ваш код...
+  for (let loopvar = 1; loopvar < table.rows.length; loopvar++)
+    {
+    let dataavailable = table.rows[loopvar].cells[3].getAttribute("data-available");
+    if (dataavailable == "true") {
+      table.rows[loopvar].classList.add('available')
+    }
+    else if (dataavailable == "false") {
+      table.rows[loopvar].classList.add('unavailable')
+    }
+    else if (dataavailable == null) {
+      table.rows[loopvar].setAttribute('hidden', '')
+    }
+    let gender = table.rows[loopvar].cells[2].innerText;
+    if (gender == "m") {
+      table.rows[loopvar].classList.add('male')
+    }
+    else if (gender == "f") {
+      table.rows[loopvar].classList.add('female')
+    }
+
+    if (parseInt(table.rows[loopvar].cells[1].innerText) < 18) {
+      table.rows[loopvar].style.textDecoration = "line-through"
+
+    }
+  }
 }
+
+
+
+
+
